2edeb8333035e6aa4e0d4e276ab1cafcd6b6a345
Cale <caleworkman@gmail.com>
2026-02-09 09:43:44 -0600

    notes from meeting

--- deepxube/domains/symbolic_regression.py
+++ deepxube/domains/symbolic_regression.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Tuple
 
-from deepxube.base.domain import State, Action, Goal, ActsEnumFixed, StartGoalWalkable, StringToAct, A
+from deepxube.base.domain import State, Action, Goal, ActsEnumFixed, StartGoalWalkable, StringToAct, StateGoalVizable
 from deepxube.factories.domain_factory import domain_factory
 
 from sympy import Expr, simplify, Integer, symbols
@@ -22,9 +22,12 @@ class SymbolicState(State):
 
 
 # TODO: Question. Why does Goal need to be its own class? Shouldn't it just be a state?
+# represents a set of states
 class SymbolicGoal(Goal):
     def __init__(self, expression: Expr):
         self.f = expression
+        # implemented incorrectly
+        # goal here would be defined by the data that I want to do symbolic regression on
 
 
 class SymbolicAction(Action):
@@ -45,22 +48,39 @@ class SymbolicAction(Action):
 
 @domain_factory.register_class('symbolic_regression')
 class SymbolicRegression(
-    ActsEnumFixed[SymbolicState, SymbolicAction, SymbolicGoal],
+    ActsEnumFixed[SymbolicState, SymbolicAction, SymbolicGoal], # ActsEnum; can limit to things like doubling or halving
     StartGoalWalkable[SymbolicState, SymbolicAction, SymbolicGoal],
+    StateGoalVizable[SymbolicState, SymbolicAction, SymbolicGoal],
     StringToAct[SymbolicState, SymbolicAction, SymbolicGoal]
 ):
 
-    def __init__(self):
+    def __init__(self, num_start_states: int):
         super().__init__()
         self.actions_fixed: List[SymbolicAction] = [SymbolicAction(x) for x in [0]]
+        # random walk from 0 - 10, sampling start states
+        # symbolic_regression.10; need to implement parser class for this see GridParser for example
 
     def sample_start_states(self, num_states: int) -> List[SymbolicState]:
         # only start from empty function (0) for now
         return num_states * [SymbolicState(Expr(Integer(0)))]
+        # want variety fo start/goal pairs
+        # use all the intermediate states from walk (search)
+    # sample start state, random walk, pick goal state
+    #
 
     def sample_goal_from_state(self, states_start: Optional[List[SymbolicState]], states_goal: List[SymbolicState]) -> List[SymbolicGoal]:
         # Implemented like Grid. I'm not sure why this is necessary since it just copies States into Goals?
         return [SymbolicGoal(state_goal.f) for state_goal in states_goal]
+        # one input variable
+        # sample inputs, add noise (assume no noise for now); evaluate the state at some values (assume 0 - 1 for input for now)
+        # fixed number of data points for simplicity
+        # e.g. 20 x's --> 20 y's for set of next data points
+
+        # simpler: assume 20 evenly spaced data points
+        # NN takes in the 20 y points, maybe normalize
+
+
+    # pickle file has the start states and goals for examples
 
     def get_actions_fixed(self) -> List[SymbolicAction]:
         return self.actions_fixed.copy()
@@ -68,7 +88,7 @@ class SymbolicRegression(
     def next_state(self, states: List[SymbolicState], actions: List[SymbolicAction]) -> Tuple[List[SymbolicState], List[float]]:
         states_next: List[SymbolicState] = []
 
-        # Should we simplify the expressions every time?
+        # Should we simplify the expressions every time? Exploit this later; for now do not simplify
 
         for state, action in zip(states, actions):
             # temporary: just increase exponent
@@ -78,19 +98,20 @@ class SymbolicRegression(
                         SymbolicState(state.f + symbols('x'))
                     )
                 else:
-                    # increment the exponent
                     states_next.append(
                         SymbolicState(state.f * symbols('x'))
                     )
-        # just using cost of 1 for now
+        # just using cost of 1 for now; this is fine, shortest path to the good expression should be simplest
         return states_next, [1.0] * len(states_next)
 
     def is_solved(self, states: List[SymbolicState], goals: List[SymbolicGoal]) -> List[bool]:
         # This assumes we know which function we're trying to get to, but shouldn't this evaluate the function
         # and see if it equals the sample values (i.e. the numbers)
         return [simplify(state.f - goal.f) == 0 for state, goal in zip(states, goals)]
+        # this is wrong
+        # should be: our state function evaluates to the data points within some tolerance (Hyperparameter)
 
-    def string_to_action(self, act_str: str) -> Optional[A]:
+    def string_to_action(self, act_str: str) -> Optional[SymbolicAction]:
         if act_str in {"0"}:
             return SymbolicAction(int(act_str))
         else:
@@ -99,10 +120,15 @@ class SymbolicRegression(
     def string_to_action_help(self) -> str:
         return "0 to increase the exponent"
 
+    def visualize_state_goal(self, state: SymbolicState, goal: SymbolicGoal, fig: Figure) -> None:
+        pass
+        # can create a figure with the data points, and the function (state) overlaid
+
+
 
 # SymPy Notes
 # evaluate expressions at a point by substituting a constant for a variable:
-#   >> expr.subs(x, 0) to evaluate f(x=0)     # this returns a new expression
+#   >> expr.subs(x, 0) to evaluate f(x=0)     # subs returns a new expression
 #   >> expr.subs([(x, 2), (y, 3)])            # multiple variables
 
 # evaluate a numerical expression like sqrt(2)
