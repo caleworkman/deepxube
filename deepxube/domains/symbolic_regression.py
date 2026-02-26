86b609b48134b21e449ca5c4113405cc1110985c
Cale <caleworkman@gmail.com>
2026-02-25 22:22:55 -0600

    random walk, some actions. need to expand on this. Add function expression to viz

--- deepxube/domains/symbolic_regression.py
+++ deepxube/domains/symbolic_regression.py
@@ -12,6 +12,8 @@ from sympy.abc import x
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
+from deepxube.utils.timing_utils import Times
+
 
 # pickle file has the start states and goals for examples
 
@@ -66,12 +68,9 @@ class SymbolicRegression(
         # Test: python -m deepxube viz --domain symbolic_regression.1
         super().__init__()
         self.random_walk_length = random_walk_length
-        self.actions_fixed = [SymbolicAction(n) for n in range(0, 4)]
 
     def sample_start_states(self, walk_length: int) -> List[SymbolicState]:
-        states = [SymbolicState(x)]
-        for _ in range(walk_length):
-            states, _ = self.next_state(states, self.actions_fixed)
+        states, costs = self.random_walk([SymbolicState(x)], [walk_length])
         return states
 
     def sample_goal_from_state(self, states_start: Optional[list[SymbolicState]], states_goal: list[SymbolicState]) -> list[SymbolicGoal]:
@@ -86,34 +85,55 @@ class SymbolicRegression(
             goals.append(SymbolicGoal(xs=xs, ys=ys, tolerance=0))
         return goals
 
+    # TODO: How to implement it so the start state and the goal state in the example are different?
+    # def sample_start_goal_pairs(self, num_steps_l: list[int], times: Optional[Times] = None) -> tuple[list[SymbolicState], list[SymbolicGoal]]:
+    #     print('pairs')
+    #     start_states = [SymbolicState(x)]
+    #     goals = self.random_walk(start_states, num_steps_l)
+    #     return start_states, goals
+
     def get_actions_fixed(self) -> List[SymbolicAction]:
-        return self.actions_fixed.copy()
+        return [SymbolicAction(n) for n in range(0, 5)]
 
     def next_state(self, states: list[SymbolicState], actions: list[SymbolicAction]) -> tuple[list[SymbolicState], list[float]]:
         states_next: List[SymbolicState] = []
 
         for state, action in zip(states, actions):
-            # choose which term to work on
+            # Terms will be length 0 if there is one term, and >2 if there are more terms. Never 1.
             terms = list(state.expr.args)
-
-            # Terms will be 0 if there is one term, and >2 if there are more terms. Never 1.
+            print('terms', terms)
             if len(terms) > 1:
+                print('more than 1')
                 idx = randint(0, len(terms)-1)
-
-                if action.action == 0:
-                    terms[idx] = terms[idx] + x
-                elif action.action == 1:
-                    terms[idx] = terms[idx] * 2
-                elif action.action == 2:
-                    terms[idx] = terms[idx] * 1/2
-                elif action.action == 3:
-                    terms[idx] = terms[idx] * x
+                terms[idx] = self._apply_action(terms[idx], action.action)
                 new_expr = state.expr.func(*terms)
             else:
-                new_expr = state.expr + 0.01
+                print('one term')
+                new_expr = self._apply_action(state.expr, action.action)
 
             states_next.append(SymbolicState(new_expr))
         return states_next, [1.0] * len(states_next)
+
+    @staticmethod
+    def _apply_action(term, action: int):
+        """Apply a sympy manipulation to a term of an expression."""
+        if action == 0:
+            # Add an integer
+            return term + 1
+        elif action == 1:
+            # Add an x
+            return term + x
+        elif action == 2:
+            # Multiply
+            return term * 2
+        elif action == 3:
+            # Divide
+            return term * 1 / 2
+        elif action == 4:
+            # Increase degree of term (e.g. x -> x^2)
+            return term * x
+        else:
+            raise ValueError('Bad action choice')
 
     def is_solved(self, states: list[SymbolicState], goals: list[SymbolicGoal]) -> list[bool]:
         # Evaluate each state at the x's in goals, and see if the y's match within some tolerance.
@@ -129,16 +149,17 @@ class SymbolicRegression(
 
     def string_to_action(self, act_str: str) -> Optional[SymbolicAction]:
         # Just for visualization
-        if act_str in {'0', '1', '2', '3'}:
+        if act_str in {'0', '1', '2', '3', '4'}:
             return SymbolicAction(int(act_str))
         else:
             return None
 
     def string_to_action_help(self) -> str:
-        return ("0 to add x\n"
-                "1 to multiply by 2 \n"
-                "2 to half\n"
-                "3 to multiply by x")
+        return ("0 to add 1\n"
+                "1 to add x\n"
+                "2 to multiply by 2 \n"
+                "3 to half\n"
+                "4 to multiply by x")
 
     def visualize_state_goal(self, state: SymbolicState, goal: SymbolicGoal, fig: Figure) -> None:
         # can create a figure with the data points, and the function (state) overlaid
